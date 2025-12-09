e13b9b5f307a70255aedcce4154fe9ea71ff0c4b
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-12-09 10:42:53 -0300

    Search bar - Part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Catalog from './routes/Catalog'
 
 export default function App() {
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,8 +1,22 @@
 import Header from "../../components/Header";
+import SearchBar from "../../components/SearchBar";
 import "./style.css";
 
 export default function Catalog() {
-    return (
-        <Header />
-    );
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section">
+          <div className="ct-search-bar-container">
+            <SearchBar />
+          </div>
+          <div className="ct-catalog-cards-container">
+            <div>Test</div>
+
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
